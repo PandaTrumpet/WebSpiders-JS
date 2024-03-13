077689da5cc86f00df1af7eb4fdbdb4383609691
PandaTrumpet <dsavontrumpet@gmail.com>
2024-03-13 23:06:24 +0200

    create prokect

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/about-me';
+import './js/benefits';
+import './js/covers';
+import './js/faq';
+import './js/footer-work-together';
+import './js/header';
+import './js/hero';
+import './js/projects';
